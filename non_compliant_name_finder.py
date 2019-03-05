2657c820bc6a27947508b39a4bddd57117ac9d6e
Pete Connolly <peteconnolly1@gmail.com>
2019-03-05 16:59:26 +0000

    Rename various variables

--- non_compliant_name_finder.py
+++ non_compliant_name_finder.py
@@ -7,7 +7,6 @@ from typing import Dict, Union
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
