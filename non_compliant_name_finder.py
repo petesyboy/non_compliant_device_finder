b467bad07d7b2a6873d1a06f1f4df306a6a5d3c8
Pete Connolly <peteconnolly1@gmail.com>
2019-11-21 19:56:28 +0000

    Continued reformat of ExtraHop functions to separate module. Code now runs again!
    Signed-off-by: Pete Connolly <peteconnolly1@gmail.com>

--- non_compliant_name_finder.py
+++ non_compliant_name_finder.py
@@ -1,195 +1,18 @@
 __author__ = 'Pete Connolly (pconnolly@extrahop.com)'
 # non_compliant_name_finder.py version 1.0
-import argparse
+
 import json
-import os
-import sys
 import time
-from pprint import pprint
 
-import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import ehoplib as eh
 
-# Disable the warning for the default self-signed certificate (I know, I know)
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-
-def get_api_key_path():
-    key_file = '.extrahop'
-    path: Union[bytes, str] = os.path.join(os.path.expanduser('~'), key_file)
-    if options.verbose:
-        print(f'Working with key file at {path}')
-
-    return path
-
-
-def read_api_key_file():
-    path = get_api_key_path()
-    create_key_file_if_needed()
-    key_dict = {}
-    with open(path, 'r+') as fh:
-        for line in fh:
-            (this_ip, key) = line.split()
-            key_dict[this_ip] = key
-    return key_dict
-
-
-def add_api_key_to_file(ip, key):
-    path = get_api_key_path()
-    create_key_file_if_needed()
-    print(f'Saving key to {path}')
-    key_dict = read_api_key_file()
-    key_dict[ip] = key
-    with open(path, 'w') as fh:
-        for this_ip, this_key in key_dict.items():
-            fh.write(str(this_ip) + ' ' + str(this_key) + '\n')
-    return
-
-
-def check_file_for_api_key(ip):
-    path = get_api_key_path()
-    create_key_file_if_needed()
-    key_dict = {}
-    with open(path, 'r+') as fh:
-        for line in fh:
-            (this_ip, key) = line.split()
-            key_dict[this_ip] = key
-
-    if ip in key_dict:
-        return key_dict[ip]
-    else:
-        return 3
-
-
-def create_key_file_if_needed():
-    path = get_api_key_path()
-    exists = os.path.isfile(path)
-    if not exists:
-        fh = open(path, 'w+')
-        fh.close()
-    # chmod(path, 0x755)
-    return
-
-
-def get_version_and_platform(ip):
-    # Grab the ExtraHop device platform and firmware version.
-    platform_url = "extrahop/"
-    extrahop_platform = call_extrahop(platform_url, "get", "")
-    eh_platform = extrahop_platform['platform']
-    firmware = extrahop_platform['version']
-    if eh_platform == 'extrahop':
-        this_platform = 'EDA'
-    else:
-        this_platform = eh_platform
-    if options.verbose:
-        print(f'ExtraHop Appliance at {ip} is an {this_platform} and the firmware version is {firmware}')
-    return this_platform, firmware
-
-
-def get_options():
-    parser = argparse.ArgumentParser(description=f'Usage: %(prog)s [options]',
-                                     epilog='Example: python3 %(prog)s -H 192.168.0.16 -v -o devices -r ^ASUS -O 100 -l 100')
-    parser.add_argument('-H', '--host',
-                        required=True,
-                        default='extrahop',
-                        help='IP or hostname of ExtraHop appliance')
-    parser.add_argument('-a', '--apikey',
-                        required=False,
-                        help='API token obtained from the ExtraHop system')
-    parser.add_argument('-o', '--outputfile',
-                        required=False,
-                        default='non_compliant_device_names',
-                        help='Name of the file to save the results to (default: non_compliant_device_names)')
-    parser.add_argument('-d', '--days',
-                        required=False,
-                        default='7',
-                        type=int,
-                        help='Number of days of lookback history to search (default: 7 days)')
-    parser.add_argument('-r', '--regex',
-                        required=False,
-                        default='^VMware',
-                        help='The RegEx pattern to use in the device name search (default "^VMware"')
-    #    parser.add_argument('-i', '--ipaddr',
-    #                        required=False,
-    #                        help='Only include devices with an IP address(L3)')  ## TODO - Implement the L3 only search
-    parser.add_argument('-l', '--limit',
-                        required=False,
-                        default=100,
-                        type=int,
-                        help='Limit the number of results (for pagination) (default: 100)')
-    parser.add_argument('-O', '--offset',
-                        required=False,
-                        type=int,
-                        default=0,
-                        help='Offset to search from (for pagination - use with the -l/--limit switch (default: 0)')
-    group = parser.add_mutually_exclusive_group()  # Need to check the syntax for the API call
-    group.add_argument('-v', '--verbose', action='store_true')  # and move the filter to a 'rule' section
-    group.add_argument('-q', '--quiet', action='store_true')
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-    options = parser.parse_args()
-    return options
-
-
-def call_extrahop(url, code, data):
-    if not options.apikey:
-        print('No API key available in call_extrahop(). Exiting')
-        exit(2)
-
-    headers = {'Accept': 'application/json',
-               'Authorization': 'ExtraHop apikey={}'.format(options.apikey)}
-    fullurl = "https://" + options.host + "/api/v1/" + url
-    if options.verbose:
-        print(f'Sending a {code} request to {fullurl} with the headers {headers}')
-
-    if code == 'get':
-        response = requests.get(fullurl, headers=headers,
-                                timeout=10, verify=False)
-    elif code == 'post':
-        if options.verbose:
-            pprint(data)
-        response = requests.post(fullurl, data=json.dumps(
-            data), headers=headers, timeout=10, verify=False)
-
-    if response.status_code != 200:
-        # response.raise_for_status()
-        if response.status_code == 401:
-            path = get_api_key_path()
-            print(
-                f'The API returned an unauthorised / missing API key error {response.status_code}. ' \
-                    f'Please check your API key in the file {path}')
-
-        elif response.status_code == 402:
-            print(
-                f'The EULA has not been accepted for this appliance (status {response.status_code}. ' \
-                f'Please browse to https://{options.host}/admin to accept the EULA')
-
-        elif response.status_code == 404:
-            print(
-                f'The requested resource could not be found. Are you specifying the right object ID? ' \
-                f'(device/appliance/alert/detection etc). {response.status_code}')
-
-        elif response.status_code == 403:
-            print(f'The current user has insufficient privileges to perform that operation. {response.status_code}')
-
-        elif response.status_code == 422:
-            print(f'Partial update or ticketing is disabled {response.status_code}')
-
-        elif response.status_code >= 500:
-            print(f'Internal Server Error. {response.status_code}')
-
-    # Return the response as a JSON object.
-    return response.json()
-
-
-options = get_options()
+options = eh.get_options()
 if not options.host:
     parser.error('Incorrect number of arguments. Specify an ExtraHop appliance IP or hostname as a minimum, '
                  'or specify "-h" for help on all options')
 
 if not options.apikey:
-    this_api_key = check_file_for_api_key(options.host)
+    this_api_key = eh.check_file_for_api_key(options.host)
     if this_api_key != 3:
         if options.verbose:
             print(f'Found API key in key file for appliance at {options.host}')
@@ -199,10 +22,10 @@ if not options.apikey:
         this_api_key = input('Please enter the API key for the specified appliance: ')
         options.apikey = this_api_key
 
-        api_key_file = get_api_key_path()
+        api_key_file = eh.get_api_key_path()
         save_key = input(f'Do you wish to save this key to the default file ({api_key_file})?')
         if save_key.lower() == "y" or save_key.lower() == 'yes':
-            add_api_key_to_file(options.host, this_api_key)
+            eh.add_api_key_to_file(options.host, this_api_key)
 
 # Name of the csv file we will be writing to....
 csvName = options.outputfile + ".csv"
@@ -211,13 +34,13 @@ file = open(csvName, "w")
 now = time.strftime("%c")
 # date and time representation
 now_for_file = time.strftime("%c")
-header_info_line = '# Generated from ExtraHop EDA at {}. Non-compliant server names in the last {} days as at {}.\n'.format(
+header_info_line = '# Generated from ExtraHop EDA at {}. Non-compliant device names in the last {} days as at {}.\n'.format(
     str(options.host), str(options.days), now_for_file)
 file.write(header_info_line)
 # Write headers of CSV file.
 file.write("device API id, Default Name, IPAddress, Display Name, MAC Address\n")
 
-platform, version = get_version_and_platform(options.host)
+platform, version = eh.get_version_and_platform(options.host, options.apikey, options.verbose)
 
 if __name__ == '__main__':
     daysinMS = int(options.days) * 86400000
@@ -266,9 +89,11 @@ if __name__ == '__main__':
         print('Filter constructed for name check:')
         print(json.dumps(device_name_check_data, indent=2))
 
+    # def call_extrahop(host, url, code, apikey, verbose, data):
     non_compliant_device_name_url = "devices/search"
-    non_compliant_device_list = call_extrahop(
-        non_compliant_device_name_url, "post", device_name_check_data)
+    non_compliant_device_list = eh.call_extrahop(options.host,
+                                                 non_compliant_device_name_url, "post", options.apikey, options.verbose,
+                                                 device_name_check_data)
 
     device: object
     cnt = 0
